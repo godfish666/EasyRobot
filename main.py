360a56c88396e667be47e01d46fc3626618378a0
luru <luru9188@126.com>
2019-03-06 16:54:39 +0800

    init

---
+++ main.py
@@ -0,0 +1,4 @@
+from til.serial.COM import COM
+
+c=COM()
+c.set_config()
---
+++ til/serial/COM.py
@@ -0,0 +1,19 @@
+class COM:
+    '''通讯接口'''
+    def __init__(self):
+        print("test")
+
+    def set_config(self):
+        print("test")
+
+    def send(self):
+        print()
+
+    def receive(self):
+        print()
+
+    def open(self):
+        print()
+
+    def close(self):
+        print()
